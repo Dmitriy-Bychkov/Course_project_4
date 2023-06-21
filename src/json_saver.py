1b72af09d0198b50e2205d99b2bce3a0a97761c3
Dmitriy Bychkov <582620@gmail.com>
2023-06-21 14:34:25 +0800

    Rewrite load_from_file method and add sort_by_max_salary method

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,40 +1,99 @@
 import json
 
-from src.hh_api import HHApi
 from src.vacancies import Vacancies
 
 
-# class JsonSaver:
-#     """Работа с файлом json"""
+class JsonSaver:
+    """Работа с файлом json"""
 
-def write_json():
-    """Запись вакансий в файл json"""
+    @staticmethod
+    def write_json(filename, vacancies):
+        """Запись вакансий в файл json"""
 
-    with open('vacancies.json', 'w', encoding='utf-8') as file:
-        json_file = []
+        with open(filename, 'w', encoding='utf-8') as file:
+            json_file = []
 
-        for vac in Vacancies.vacancies:
-            vacancies_dict = {'name': vac.name,
-                              'description': vac.description,
-                              'area': vac.area,
-                              'salary': vac.salary,
-                              'currency': vac.currency,
-                              'experience': vac.experience,
-                              'employer': vac.employer,
-                              'employment': vac.employment,
-                              'vacancy_url': vac.vac_url
-                              }
+            for vac in vacancies:
+                vacancies_dict = {'name': vac.name,
+                                  'salary': vac.salary(),
+                                  'salary_min': vac.salary_from,
+                                  'salary_max': vac.salary_to,
+                                  'currency': vac.currency,
+                                  'experience': vac.experience,
+                                  'employment': vac.employment,
+                                  'requirement': vac.requirement,
+                                  'responsibility': vac.responsibility,
+                                  'description': vac.description(),
+                                  'area': vac.area,
+                                  'employer': vac.employer,
+                                  'vacancy_url': vac.vac_url()
+                                  }
 
-            json_file.append(vacancies_dict)
-        file.write(json.dumps(json_file, ensure_ascii=False))
+                json_file.append(vacancies_dict)
+            file.write(json.dumps(json_file, sort_keys=False, indent=4, ensure_ascii=False))
 
+    @staticmethod
+    def load_from_file(filename):
+        """Чтение вакансий из файл json"""
 
-a = HHApi()
-a.keyword = 'python'
-b = a.get_vacancies()
-print(b)
-c = Vacancies.vacancies
-print('-----')
-print(c)
-print('-----')
-d = write_json()
+        with open(filename, 'r', encoding='UTF-8') as file:
+            json_data = file.read()
+            data = json.loads(json_data)
+        vacancies = []
+        for i in data:
+            name = i["name"]
+            requirement = i["requirement"]
+            responsibility = i["responsibility"]
+            salary_from = i["salary_min"]
+            salary_to = i["salary_max"]
+            currency = i["currency"]
+            experience = i["experience"]
+            employer = i["employer"]
+            employment = i["employment"]
+            area = i["area"]
+            vacancy_url = i["vacancy_url"]
+
+            vacancy = Vacancies(
+                name,
+                requirement,
+                responsibility,
+                salary_from,
+                salary_to,
+                currency,
+                experience,
+                employer,
+                employment,
+                area,
+                vacancy_url
+            )
+            vacancies.append(vacancy)
+
+        return vacancies
+
+    def sort_by_max_salary(self, vacancies):
+        """Сортировка вакансий по наибольшей зарплате"""
+
+        max_salary_list = sorted(vacancies,
+                                 key=lambda x: x.get('salary_max') if x.get('salary_max') != 0 else x.get('salary_min'),
+                                 reverse=True)
+        # max_salary_list = sorted(vacancies, reverse=True)
+
+        return max_salary_list
+
+# a = JsonSaver()
+# b = a.load_from_file()
+# c = a.sort_by_max_salary()
+# print(b)
+# print(c)
+# for res in c:
+#     print(res["name"], '-', res["salary"])
+# a = HHApi()
+# a.per_page = 3
+# a.keyword = 'java'
+# a.get_vacancies()
+# # print(b)
+# #c = Vacancies.vacancies
+# # print('-----')
+# # print(c)
+# # print('-----')
+# write_json()
